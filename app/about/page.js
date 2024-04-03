029a93430f56a6a29fd0fb16fde3d757d2ace52c
Richard McSharry <rmcsharry@gmail.com>
2024-04-03 14:04:51 +0200

    add posthog NodeJs client, test server-side events

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,15 @@
 import Link from 'next/link'
+import PostHogClient from '../posthog'
 
 export default function About() {
+
+  const posthog = PostHogClient()
+
+  posthog.capture({
+    distinctId: 'test@posthog.com', // replace with a user's distinct ID
+    event: 'server_side_visited_about_page'
+  })
+
   return (
     <main>
       <h1>About</h1>
